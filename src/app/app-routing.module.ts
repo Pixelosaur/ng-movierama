d68c2bb4fd7428a9ecd6d2242f43c9f6be74a71e
christina <xristitina@gmail.com>
2019-09-14 14:28:36 +0300

    sets up app routing module

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,24 @@
+import { RouterModule, Routes } from '@angular/router';
+import { AuthGuard } from './core/auth/guards/auth.guard';
+import { MoviesComponent } from './movies/components/movies/movies.component';
+import { LoginComponent } from './login/components/login/login.component';
+import { NgModule } from '@angular/core';
+import { LayoutComponent } from './layout/components/layout.component';
+
+const routes: Routes = [
+    { path: '', redirectTo: 'login', pathMatch: 'full' },
+    { path: 'login', component: LoginComponent },
+    {
+        path: '',
+        component: LayoutComponent,
+        canActivate: [AuthGuard],
+        runGuardsAndResolvers: 'always',
+        children: [{ path: 'movies', component: MoviesComponent }],
+    },
+];
+
+@NgModule({
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule],
+})
+export class AppRoutingModule {}
