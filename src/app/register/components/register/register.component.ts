9292c52faba9aff0d0db42c8d64996df4bd963ed
christina <xristitina@gmail.com>
2019-09-15 22:00:15 +0300

    implements register functionality on register component

--- src/app/register/components/register/register.component.ts
+++ src/app/register/components/register/register.component.ts
@@ -1,11 +1,59 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { RegisterUser } from '../../interfaces/register-user.interface';
+import { RegisterService } from '../../services/register.service';
 
 @Component({
     selector: 'app-register',
     templateUrl: './register.component.html',
+    styleUrls: ['./register.component.css'],
 })
 export class RegisterComponent implements OnInit {
-    constructor() {}
+    // error
+    isAlertDisplayed: boolean = false;
+    errorMessage: string;
 
-    ngOnInit(): void {}
+    // form
+    registerForm: FormGroup;
+
+    // Form controls validation patterns
+    emailValidationPattern: string = `^(([^<>()\\[\\]\\\\.,;:\\s@"]+(\\.[^<>()\\[\\]\\\\.,;:\\s@"]+)*)|(".+"))@((\\[[0-9]{1,3}\\.[0-9]{1,3}\\.[0-9]{1,3}\\.[0-9]{1,3}])|(([a-zA-Z\\-0-9]+\\.)+[a-zA-Z]{2,}))`;
+
+    constructor(
+        private formBuilder: FormBuilder,
+        private router: Router,
+        private registerService: RegisterService,
+    ) {}
+
+    ngOnInit(): void {
+        this.initRegisterForm();
+    }
+
+    /* initialize form values and set up validators */
+    initRegisterForm(): void {
+        this.registerForm = this.formBuilder.group({
+            name: [null, [Validators.required]],
+            username: [
+                null,
+                [Validators.required, Validators.pattern(this.emailValidationPattern)],
+            ],
+            password: [null, [Validators.required]],
+            confirmPassword: [null, [Validators.required]],
+        });
+    }
+
+    onRegister(user: RegisterUser): void {
+        this.registerService.createUser(user).subscribe(
+            (registerUser: RegisterUser) => this.router.navigate(['/login']),
+            (error: any) => {
+                // on error
+                this.errorMessage = error.message;
+                this.isAlertDisplayed = true;
+
+                // hide the alert after 15 seconds
+                setTimeout(() => (this.isAlertDisplayed = false), 15000);
+            },
+        );
+    }
 }
