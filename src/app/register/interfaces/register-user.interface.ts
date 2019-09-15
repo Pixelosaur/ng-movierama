3ae46917cdbe30c783f6acd46e3fca58c3b5026a
christina <xristitina@gmail.com>
2019-09-15 22:00:40 +0300

    modifies registerUser interface

--- src/app/register/interfaces/register-user.interface.ts
+++ src/app/register/interfaces/register-user.interface.ts
@@ -2,5 +2,6 @@ import { LoginUser } from '../../login/interfaces/login-user.interface';
 
 export interface RegisterUser extends LoginUser {
     name: string;
+    id?: string;
     confirmPassword: string;
 }
